8a3892b9fe79e36b786516d5f60b34c0e40b8886
Lee0701 <hgl1002@naver.com>
2020-03-04 11:19:54 +0900

    Add render on fetch

--- public/index.js
+++ public/index.js
@@ -4,22 +4,33 @@ $(document).ready(function() {
         event.preventDefault()
         $.ajax({
             url: '/component/cangjie/' + $('#cangjie > input').val(),
-            success: (data) => $('#editor > textarea').val(JSON.stringify(data, null, 4))
+            success: (data) => {
+                $('#editor > textarea').val(JSON.stringify(data, null, 4))
+                render()
+            }
         })
     })
     $('#code').submit(function(event) {
         event.preventDefault()
         $.ajax({
             url: '/component/code/' + $('#code > input').val(),
-            success: (data) => $('#editor > textarea').val(JSON.stringify(data, null, 4))
+            success: (data) => {
+                $('#editor > textarea').val(JSON.stringify(data, null, 4))
+                render()
+            }
         })
     })
     $('#editor').submit(function(event) {
         event.preventDefault()
+        render()
+    })
+
+    const render = function() {
         $.post({
             url: '/render/json',
             data: {component: $('#editor > textarea').val()},
             success: (data) => $('#glyph-view').removeAttr('src').attr('src', 'data:image/png;base64,' + data)
         })
-    })
+    }
+
 })
