bbbc440e46c528d347351f956c5a2d6cdd238bbe
Lee0701 <hgl1002@naver.com>
2020-03-06 20:24:35 +0900

    Add submission

--- telegram-bot.js
+++ telegram-bot.js
@@ -6,7 +6,9 @@ const Canvas = require('canvas')
 const Telegraf = require('telegraf')
 const commandParts = require('telegraf-command-parts')
 
-const cangjieLang = CangjieLang.DEFAULT
+const fs = require('fs')
+const path = require('path')
+
 const cangjie = Cangjie.DEFAULT
 const width = 128
 const height = 128
@@ -17,11 +19,15 @@ const y = margin
 const w = width - margin*2
 const h = height - margin*2
 
+const dataFile = 'data.json'
+
 class TelegramBot {
     constructor(token) {
         this.token = token
         this.bot = new Telegraf(token)
         this.bot.use(commandParts())
+
+        this.loadCangjie()
 
         this.bot.command('cangjie', (context) => {
             const args = context.state.command.args.split(/ +/)
@@ -37,7 +43,7 @@ class TelegramBot {
                 ctx.lineWidth = 6
                 ctx.lineCap = 'square'
                 try {
-                    cangjieLang.parse(ch).render(ctx, x, y, w, h)
+                    this.cangjieLang.parse(ch).render(ctx, x, y, w, h)
                 } catch(e) {
                     console.log(e)
                 }
@@ -57,7 +63,7 @@ class TelegramBot {
             ctx.lineWidth = 6
             ctx.lineCap = 'square'
             try {
-                cangjieLang.parse(arg).render(ctx, x, y, w, h)
+                this.cangjieLang.parse(arg).render(ctx, x, y, w, h)
             } catch(e) {
                 console.log(e)
             }
@@ -68,22 +74,54 @@ class TelegramBot {
 
         this.bot.command('get', (context) => {
             const arg = context.state.command.args
-            context.reply(cangjieLang.data[arg] || 'No result')
+            context.reply(this.cangjieLang.data[arg] || 'No result')
         })
 
         this.bot.command('extract', (context) => {
             const arg = context.state.command.args
-            context.reply(cangjieLang.extract(arg))
+            context.reply(this.cangjieLang.extract(arg))
         })
 
         this.bot.command('charset', (context) => {
-            context.reply(Object.keys(cangjieLang.data).join(''))
+            context.reply(Object.keys(this.cangjieLang.data).join(''))
+        })
+
+        this.bot.command('submit', (context) => {
+            const args = context.state.command.args.split(' ')
+            const name = args.shift() + context.message.from.username.toUpperCase().replace(/\_/g, '')
+            if(name.charAt(0).toUpperCase() >= 'A' && name.charAt(0).toUpperCase() <= 'Z') {
+                context.reply('Name must not start with an alphabet')
+            } else {
+                this.data[name] = args.join(' ')
+                this.saveData()
+                this.loadCangjie()
+                context.reply('Submission done, name: ' + name)
+            }
+            
         })
 
     }
+
     launch() {
         this.bot.launch()
     }
+
+    loadCangjie() {
+        try {
+            this.data = JSON.parse(fs.readFileSync(dataFile).toString())
+        } catch(error) {
+            this.data = {}
+        }
+        const data = {}
+        Object.assign(data, this.data)
+        Object.assign(data, CangjieLang.DEFAULT_DATA)
+        this.cangjieLang = new CangjieLang(data)
+    }
+
+    saveData() {
+        fs.writeFileSync(dataFile, JSON.stringify(this.data))
+    }
+    
 }
 
 module.exports = TelegramBot
